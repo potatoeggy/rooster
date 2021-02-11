8a06271cb04317ecfc8a35e5b2cba5a30f738e76
eggy <25178974+potatoeggy@users.noreply.github.com>
2021-02-11 15:58:46 -0500

    lots of refactoring, remove hammer mode

--- schoolschedule.py
+++ schoolschedule.py
@@ -7,20 +7,18 @@ import time
 import json
 import sys
 
-# TODO: get rid of global vars
-GMAIL_ADDRESS, YRDSB_PASSWORD, DISCORD_URL, ADMIN_USER_ID, VERBOSE, WORKER_VISIBLE, HAMMER_MODE, HAMMER_DELAY, GECKODRIVER_PATH, GECKODRIVER_LOG, CHROMEDRIVER_PATH, CHROMEDRIVER_LOG, RENDER_BACKEND = (None for i in range(13))
+# TODO: remove all globals
+VERBOSE = True # true for now because some debug statements are called prior to VERBOSE being set
 
-def debug(string):
-	if VERBOSE:
+def debug(string, urgent=False):
+	if urgent:
+		print(string)
+	elif VERBOSE:
 		print("DEBUG:", string)
 
 # returns a datetime object now for convenience
 def now():
 	return datetime.datetime.now()
-
-# searches for a value in a container and returns a fallback if not found
-def in_container(key, fallback, container):
-	return container[key] if key in container else fallback
 
 # prints the help page and exits
 def show_help():
@@ -39,31 +37,81 @@ class Class:
 		self.link = link
 		self.enabled = enabled
 	
-	def send_discord_message(self):
-		payload = {
-			"content": "<@&{0}>, **{1}** with {2} is now **open** at <{3}> !".format(self.discord_role, self.name, self.teacher, self.link)
-		}
+	def get_discord_message(self):
+		return f"<@&{self.discord_role}>, **{self.name}** with {self.teacher} is now **open** at <{self.link}> !"
 
-		if VERBOSE:
-			print(payload)
-		else:
-			requests.post(DISCORD_URL, data=payload)
+class DiscordCommunicator:
+	def __init__(self, discord_url, admin_user_id):
+		self.discord_url = discord_url
+		self.admin_user_id = admin_user_id
+
+	def send_message(self, string):
+		if string != "":
+			debug(f"{now()}: {string}", urgent=True)
+		payload = { "content": string }
+		#requests.post(self.discord_url, data=payload)
 	
-def send_help(string="", abort=True):
-	if string != "":
-		print(string)
-	payload = { "content": f"<@!{ADMIN_USER_ID}>, manual intervention required! " + string }
-	requests.post(DISCORD_URL, data=payload)
-	if abort:
-		exit()
+	def send_help(self, string="", abort=True):
+		self.send_message(string)
+		if abort:
+			exit()
 
-# load custom config
-def load_config():
-	# TODO: get rid of this asap
-	global GMAIL_ADDRESS, YRDSB_PASSWORD, DISCORD_URL, ADMIN_USER_ID, VERBOSE, WORKER_VISIBLE, HAMMER_MODE, HAMMER_DELAY, GECKODRIVER_PATH, GECKODRIVER_LOG, CHROMEDRIVER_PATH, CHROMEDRIVER_LOG, RENDER_BACKEND
+def process_class_data(class_data, discord):
+	classes = []
+
+	# if no classes there is nothing to do
+	if len(class_data) == 0:
+		discord.send_help("Exiting because no classes found.")
+
+	# change json to object
+	for c in class_data:
+		classes.append(Class(c["name"], c["teacher"], c["start_time"], c["end_time"], c["role"], c["link"], c["enabled"]))
+	sorted_classes = sorted(classes, key=lambda c: c.start_time) # sort by time started
+	debug(f"Found {len(sorted_classes)} classes.")
+	return sorted_classes
+
+def init_driver(render_backend, worker_visible, driver_path, driver_log, discord):
+	# initialise web engine
+	if render_backend == "geckodriver": # note firefox is borked in headless
+		options = webdriver.firefox.options.Options()
+		options.headless = not worker_visible
+		profile = webdriver.FirefoxProfile()
+		profile.DEFAULT_PREFERENCES["frozen"]["dom.webdriver.enabled"] = False
+		driver = webdriver.Firefox(options=options, firefox_profile=profile, executable_path=driver_path, service_log_path=driver_log)
+	elif render_backend == "chromedriver":
+		options = webdriver.ChromeOptions()
+		if not worker_visible:
+			options.add_argument("headless")
+			options.add_argument("user-agent=\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36\"")
+		driver = webdriver.Chrome(options=options, executable_path=driver_path, service_log_path=driver_log)
+	else:
+		discord.send_help(f"ERROR: {render_backend} is not a valid render backend.")
+	return driver
+
+def login_google(gmail_address, yrdsb_password, driver):
+	# login to google to use lookup links
+	driver.get("https://accounts.google.com/ServiceLogin?continue=https://google.com")
+	driver.implicitly_wait(10)
+	driver.find_element_by_id("identifierId").send_keys(gmail_address) # input email
+	driver.find_element_by_id("identifierNext").click()
+
+	driver.find_element_by_id("UserName").send_keys(gmail_address.split("@")[0]) # YRDSB login field
+	driver.find_element_by_id("Password").send_keys(yrdsb_password)
+	driver.find_element_by_id("LoginButton").click()
+
+	time.sleep(3) # google is slow
+
+	if "speedbump" in driver.current_url:
+		driver.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button').click()
+		debug("speedbump passed")
+
+def init():
+	global VERBOSE
+	debug("Initialising program...")
+	# check command line arguments, config file, fallback
 	def check_config(key, fallback):
-		# TODO: check if environment variable is set and use that?
-		result = in_container(key, fallback, obj) # config file has lowest priority
+		# TODO: consider environment variables
+		result = obj[key] if key in obj else fallback
 		args_key = "--" + key.replace("_", "-")
 		try:
 			if type(fallback) is not bool:
@@ -76,162 +124,109 @@ def load_config():
 			show_help()
 		return result
 	
-	CONFIG_FILE = "config.json"
+	# read from config file
+	config_file = "config.json"
 	try:
-		CONFIG_FILE = sys.argv[sys.argv.index("--config") + 1]
+		debug("Reading configuration...")
+		config_file = sys.argv[sys.argv.index("--config") + 1]
 	except ValueError:
 		pass # user did not specify custom conf location
 	except IndexError:
 		show_help()
 
 	# load config from file
-	with open(CONFIG_FILE, "r") as file:
+	with open(config_file, "r") as file:
 		data = file.read()
-
 	obj = json.loads(data)
 
-	# TODO: move all non-necessary global fields to be local instead being passed around
 	# mandatory fields
-	GMAIL_ADDRESS = obj["gmail_address"] # gapps account
-	YRDSB_PASSWORD = obj["yrdsb_password"] # password in plaintext :P
-	DISCORD_URL = obj["discord_url"] # discord webhook url
-	ADMIN_USER_ID = obj["admin_user_id"] # discord user id to ping in emergencies
+	gmail_address = obj["gmail_address"] # gapps account
+	yrdsb_password = obj["yrdsb_password"] # password in plaintext :P
+	discord_url = obj["discord_url"] # discord webhook url
+	admin_user_id = obj["admin_user_id"] # discord user id to ping in emergencies
 
 	# optional fields
 	VERBOSE = check_config("verbose", False) # if debug statements are printed
-	WORKER_VISIBLE = check_config("worker_visible", False) # if browser window is visible
-	HAMMER_MODE = check_config("hammer_mode", True) # if start time and end time should be ignored
-	HAMMER_DELAY = check_config("hammer_delay", 20) # delay between pings in hammer mode
-	GECKODRIVER_PATH = check_config("geckodriver_path", "/usr/bin/geckodriver")
-	GECKODRIVER_LOG = check_config("geckodriver_log", "./geckodriver.log")
-	CHROMEDRIVER_PATH = check_config("chromedriver_path", "/usr/bin/chromedriver")
-	CHROMEDRIVER_LOG = check_config("chromedriver_log", "./chromedriver.log")
-	RENDER_BACKEND = check_config("render_backend", "chromedriver") # one of "chromedriver" or "geckodriver"
-
+	worker_visible = check_config("worker_visible", False) # if browser window is visible
+	# TODO: consider removing two paths/logs since you can only use one anyway
+	render_backend = check_config("render_backend", "chromedriver") # one of "chromedriver" or "geckodriver"
+	driver_path = check_config(f"{render_backend}_path", f"/usr/bin/{render_backend}")
+	driver_log = check_config(f"{render_backend}_log", f"./{render_backend}.log")
 	class_data = obj["class_data"]
 
-	# if no classes there is nothing to do
-	if len(class_data) == 0:
-		send_help("Exiting because no classes found.")
-	
-	return class_data
+	discord = DiscordCommunicator(discord_url, admin_user_id)
+	debug("Processing class data...")
+	sorted_classes = process_class_data(class_data, discord)
+	debug("Initialising browser...")
+	driver = init_driver(render_backend, worker_visible, driver_path, driver_log, discord)
+	debug("Authenticating with Google...")
+	login_google(gmail_address, yrdsb_password, driver)
+	debug("Initialisation complete.")
+	return (sorted_classes, driver, discord)
 
-def process_class_data(class_data):
-	classes = []
-	sorted_classes = []
-
-	# change json to object
-	if not HAMMER_MODE:
-		for c in class_data:
-			classes.append(Class(c["name"], c["teacher"], c["start_time"], c["end_time"], c["role"], c["link"], c["enabled"]))
-		sorted_classes = sorted(classes, key=lambda c: c.start_time) # sort by time started
-	else:
-		for c in class_data:
-			classes.append(Class(c["name"], c["teacher"], None, None, c["role"], c["link"], c["enabled"]))
-
-		sorted_classes = classes # pass it over since no sorting needed while hammer
-	return classes, sorted_classes
-
-def init_driver():
-	# initialise web engine
-	if RENDER_BACKEND == "geckodriver": # note firefox is borked in headless
-		options = webdriver.firefox.options.Options()
-		options.headless = not WORKER_VISIBLE
-		profile = webdriver.FirefoxProfile()
-		profile.DEFAULT_PREFERENCES["frozen"]["dom.webdriver.enabled"] = False
-		driver = webdriver.Firefox(options=options, firefox_profile=profile, executable_path=GECKODRIVER_PATH, service_log_path=GECKODRIVER_LOG)
-	elif RENDER_BACKEND == "chromedriver":
-		options = webdriver.ChromeOptions()
-		if not WORKER_VISIBLE:
-			options.add_argument("headless")
-			options.add_argument("user-agent=\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36\"")
-		driver = webdriver.Chrome(options=options, executable_path=CHROMEDRIVER_PATH, service_log_path=CHROMEDRIVER_LOG)
-	else:
-		send_help(f"ERROR: {RENDER_BACKEND} is not a valid render backend.")
-
-	# login to google to use lookup links
-	driver.get("https://accounts.google.com/ServiceLogin?continue=https://google.com")
-	driver.implicitly_wait(10)
-	driver.find_element_by_id("identifierId").send_keys(GMAIL_ADDRESS) # input email
-	driver.find_element_by_id("identifierNext").click()
-
-	driver.find_element_by_id("UserName").send_keys(GMAIL_ADDRESS.split("@")[0]) # YRDSB login field
-	driver.find_element_by_id("Password").send_keys(YRDSB_PASSWORD)
-	driver.find_element_by_id("LoginButton").click()
-
-	time.sleep(3) # google is slow
-
-	if "speedbump" in driver.current_url:
-		driver.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button').click()
-		debug("speedbump passed")
-	return driver
-
-def init():
-	class_data = load_config()
-	classes, sorted_classes = process_class_data(class_data)
-	driver = init_driver()
-	return classes, sorted_classes, driver
-
-def ping_meet(c, driver):
+def ping_meet(c, driver, discord):
 	driver.get(c.link)
 	html = driver.page_source
 	if not "meet.google.com" in c.link:
 		print("Zoom detection not available for {0}, sending Discord hook at first opportunity".format(c.name))
-		c.send_discord_message()
+		discord.send_message(c.get_discord_message())
 		found[i] = True
 	elif "Ready to join?" in html:
 		# meet is open
-		c.send_discord_message()
+		discord.send_message(c.get_discord_message())
 		debug("class {0} message sent".format(c.name))
 		found[i] = True
 	elif "Not your computer?" in html:
 		# not logged in even when bot is supposed to be logged in
-		send_help("ERROR: Bot is not logged in.")
+		discord.send_help("ERROR: Bot is not logged in.")
 	elif "Check your meeting code" in html or "You can't create a meeting yourself" in html:
 		# meet is not open, continue waiting
 		pass
 	elif "Your meeting code has expired" in html:
 		# right after class or link needs to be updated, or class was dismissed early
-		send_help(f"WARNING: Link needs to be updated for {c.name}", abort=False)
+		discord.send_help(f"WARNING: Link needs to be updated for {c.name}", abort=False)
 	elif "Invalid video call name" in html:
-		send_help(f"ERROR: Invalid link for {c.name}", abort=False)
+		discord.send_help(f"ERROR: Invalid link for {c.name}", abort=False)
 	elif "Getting ready" in html:
 		print("WARNING: Delay is too slow, Google is still getting ready")
 	elif "You can't join this video call" in html:
-		send_help(f"ERROR: Google bot detection triggered or not authenticated with {c.name}")
+		discord.send_help(f"ERROR: Google bot detection triggered or not authenticated with {c.name}")
 	else:
-		send_help(f"ERROR: Something unexpected happened with {c.name}")
+		discord.send_help(f"ERROR: Something unexpected happened with {c.name}")
 
+# main event
 if __name__ == "__main__":
 	if "--help" in sys.argv or "-h" in sys.argv:
 		show_help()
 
-	classes, sorted_classes, driver = init()
+	sorted_classes, driver, discord = init()
 	driver.implicitly_wait(15)
 	earliest = sorted_classes[0].start_time
 	latest = sorted_classes[-1].end_time
-	found = [c.enabled for c in sorted_classes]
+	found = [not c.enabled for c in sorted_classes]
 
 	# only run while in school hours
+	if now() > latest:
+		debug(f"Current time {now()} is after 3end of last class at {latest}.")
+	if all(found):
+		debug(f"No classes are enabled.")
+	
 	while now() < latest and not all(found):
-		if not HAMMER_MODE: # use the times given for each class
-			try:
-				# sleep until five minutes before next class
-				earliest_valid_class = sorted_classes[0]
-				for i, c in enumerate(sorted_classes):
-					if not found[i]:
-						earliest_valid_class = c
-						break
-				debug("sleeping for {0} seconds".format((earliest_valid_class.start_time-now()).total_seconds()))
-				time.sleep((earliest_valid_class.start_time-now()).total_seconds())
-			except ValueError:
-				# expected as part of regular loop (oversleep might happen but is rare)
-				pass
+		try:
+			# sleep until five minutes before next class
+			earliest_valid_class = sorted_classes[0]
+			for i, c in enumerate(sorted_classes):
+				if not found[i]:
+					earliest_valid_class = c
+					break
+			debug("sleeping for {0} seconds".format((earliest_valid_class.start_time-now()).total_seconds()))
+			time.sleep((earliest_valid_class.start_time-now()).total_seconds())
+		except ValueError:
+			# expected as part of regular loop (oversleep might happen but is rare)
+			pass
 		
 		for i, c in enumerate(sorted_classes):
 			if found[i] or not c.enabled: continue
-			if HAMMER_MODE:
-				ping_meet(c, driver)
 			elif c.end_time <= now(): # link not found for too long
 				debug("Skipped class {0} as it is past its end time".format(c.name))
 				found[i] = True
@@ -239,7 +234,8 @@ if __name__ == "__main__":
 			elif c.start_time > now(): # not start time yet, we can exit because the array should be sorted
 				break
 			elif c.start_time <= now(): # between end and start times
-				ping_meet(c, driver)
+				ping_meet(c, driver, discord)
 		
-		time.sleep(5 if not HAMMER_MODE else HAMMER_DELAY) # combined with the delay in processing and getting this should add up to about 10 s delay per ping
+		time.sleep(5) # combined with the delay in processing and getting this should add up to about 10 s delay per ping
+	debug("Exiting...")
 	driver.quit()
